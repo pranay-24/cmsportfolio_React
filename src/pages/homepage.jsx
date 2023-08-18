4dc570c82cad18d6e5b8b53a4820966e16588a83
Pranay Mangal <mangal.pranay@gmail.com>
2023-08-17 23:29:02 -0400

    dtaa

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -22,7 +22,7 @@ const Homepage = () => {
 
   useEffect(() => {
     // Fetch social media links from the JSON file
-    fetch("data.json")
+    fetch("/data.json")
       .then((response) => response.json())
       .then((data) => {
         setSocialMediaLinks(data.socialMediaLinks);
